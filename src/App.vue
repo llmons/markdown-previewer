18a4c35dd67730427497a5a5f746d4785abdffb3
llmons <llmons@foxmail.com>
2025-02-02 23:35:58 +0800

    initialize Vue 3 + TypeScript + Vite project with Tailwind CSS and Markdown previewer

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import markdownit from 'markdown-it'
+import ThemeSwap from './components/ThemeSwap.vue'
+
+const text = ref('hello')
+const marked = ref('')
+
+const md = markdownit({
+  breaks: true,
+});
+
+const updateText = () => {
+  marked.value = md.render(text.value)
+}
+
+</script>
+
+<template>
+  <div class="flex flex-col h-screen gap-5">
+    <!-- navabr -->
+    <div class="flex items-center justify-center text-4xl navbar">
+      <div class="flex items-center navbar-center">
+        Markdown Previewer
+      </div>
+      <div class="flex items-center navbar-end">
+        <ThemeSwap />
+      </div>
+    </div>
+
+    <!-- editor & previewer -->
+    <div class="container flex items-center justify-center h-full mx-auto">
+      <textarea
+        class="w-1/2 h-full p-4 text-lg border-2 rounded-lg resize-none textarea textarea-ghost focus:outline-none border-base-300 focus:border-2 focus:border-base-300"
+        v-model="text" @input="updateText"></textarea>
+      <div class="divider divider-horizontal"></div>
+      <div class="w-1/2 h-full p-4 prose border-2 rounded-lg border-base-300" v-html="marked"></div>
+    </div>
+  </div>
+</template>
